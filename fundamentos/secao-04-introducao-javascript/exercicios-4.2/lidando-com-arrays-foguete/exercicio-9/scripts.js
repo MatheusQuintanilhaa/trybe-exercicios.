c4ecfb7d38ee3182642b2cfdf43541556c6c9243
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2022-11-09 02:14:01 -0300

    Create scripts.js

---
+++ fundamentos/secao-04-introducao-javascript/exercicios-4.2/lidando-com-arrays-foguete/exercicio-9/scripts.js
@@ -0,0 +1,14 @@
+//Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+
+let resultado = [];
+
+for (let index = 1; index <= 25; index += 1) {   
+    resultado.push(index / 2) ;
+}
+
+
+console.log(resultado)
+
+
+
+
