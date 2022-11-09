2bdef1f0556fe1e287b296aa333925eae2cdca87
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2022-11-09 02:29:49 -0300

    Create script2.js

---
+++ fundamentos/secao-07-javascript-es6-let-const-arrow-function/exercicio-foguete/exercicio 2/script2.js
@@ -0,0 +1,7 @@
+/*Crie uma função que faça com que o array oddsAndEvens fique em ordem crescente;
+Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne a frase “Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!”.
+Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números. */
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`);
