6574eb8a5d18cc7b6886884c365b858c17ea3e06
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2022-11-09 02:15:32 -0300

    Create scripts.js

---
+++ fundamentos/secao-04-introducao-javascript/tipos-primitivos-tipagem-dinamica-e-operacoes-aritmeticas/foguete/scripts.js
@@ -0,0 +1,10 @@
+const base = 5;
+const height = 8;
+const area = "resultado";
+const perimeter = "soma";
+
+resultado = base * height;
+ console.log(resultado);
+
+ soma = base + base + base + base + base;
+ console.log(soma);
