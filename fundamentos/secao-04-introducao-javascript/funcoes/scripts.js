099ccaf4e03de265a94b3d1e270f8defe934135f
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2022-11-09 02:14:59 -0300

    Create scripts.js

---
+++ fundamentos/secao-04-introducao-javascript/funcoes/scripts.js
@@ -0,0 +1,11 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+
+for (const key in info) {
+   console.log(info[key])
+        
+    }
