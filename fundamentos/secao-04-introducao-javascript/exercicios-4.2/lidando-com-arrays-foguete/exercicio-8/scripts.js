7e9f5438608c40c9d851f888a011a1049b15d87e
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2022-11-09 02:13:56 -0300

    Create scripts.js

---
+++ fundamentos/secao-04-introducao-javascript/exercicios-4.2/lidando-com-arrays-foguete/exercicio-8/scripts.js
@@ -0,0 +1,9 @@
+//Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+let resultado = [];
+
+for (let index = 1; index <= 25; index += 1) {   
+    resultado.push(index) ;
+}
+
+console.log(resultado)
