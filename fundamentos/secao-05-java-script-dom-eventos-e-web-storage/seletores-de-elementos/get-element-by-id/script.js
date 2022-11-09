8ec524b5c86e8a170c54a88dc24dfb1611cdcf05
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2022-11-09 02:28:07 -0300

    Create script.js

---
+++ fundamentos/secao-05-java-script-dom-eventos-e-web-storage/seletores-de-elementos/get-element-by-id/script.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Avengers";
+document.getElementById("second-paragraph").innerText = " Este filme é baseado em uma história de heróis da marvel";
+document.getElementById("subtitle").innerText = "Sinopse";
