2b1aa03551058530f7495cf5285d1cb5200e3ff1
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2022-11-09 02:15:21 -0300

    Create scripts.js

---
+++ fundamentos/secao-04-introducao-javascript/operadores-logicos/operador-or/scripts.js
@@ -0,0 +1,8 @@
+let weekDay = "quarta-feira";
+
+if (weekDay === "segunda-feira","terça-feira","quarta-feira","quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+} else if (weekDay === "sábado","domingo") {
+    console.log("FINALMENTE, descanso merecido UwU")
+}
+
