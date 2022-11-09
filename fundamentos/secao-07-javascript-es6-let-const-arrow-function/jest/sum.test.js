ebb7e64750283fee3106ca4052e76e3703a95cdc
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2022-11-09 02:30:37 -0300

    Create sum.test.js

---
+++ fundamentos/secao-07-javascript-es6-let-const-arrow-function/jest/sum.test.js
@@ -0,0 +1,10 @@
+import jest from '@jest/globals'
+
+const {expect} = require('expect');
+const sum = require('./script');
+
+test('Verifica se a soma de 1 e 2 é 3', () => {
+  expect(sum(1, 2)).toBe(3);
+})
+
+
