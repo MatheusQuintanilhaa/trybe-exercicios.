a10695469ad6c1e1784bd087f51f6f0fbf565517
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2022-11-09 02:15:27 -0300

    Create scripts.js

---
+++ fundamentos/secao-04-introducao-javascript/switch-e-case/scripts.js
@@ -0,0 +1,14 @@
+let resultado = "aprovada";
+
+switch (resultado) {
+    case "aprovada":
+        console.log("meus parabéns");
+     break;
+     case "lista":
+        console.log("reveja os exercícios");
+        break;
+        case "reprovada":
+            console.log("Estude mais");
+     default:
+        console.log("Infelizmente não foi dessa vez");            
+}
