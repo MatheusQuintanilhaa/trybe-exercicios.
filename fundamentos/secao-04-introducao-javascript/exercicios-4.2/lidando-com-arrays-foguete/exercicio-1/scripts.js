c8b98dcc6296d133ea1b86937ec9f65296d17370
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2022-11-09 02:13:25 -0300

    Create scripts.js

---
+++ fundamentos/secao-04-introducao-javascript/exercicios-4.2/lidando-com-arrays-foguete/exercicio-1/scripts.js
@@ -0,0 +1,12 @@
+//Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media = 0;
+
+for (let index = 0; index < numbers.length; index++) {
+    console.log(numbers[index]);
+    
+}
+
+
