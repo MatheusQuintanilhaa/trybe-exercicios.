cf4f581b52c42e2846e8b27b17b5e5a96ee9c8ec
Matheus Quintanilha <matheussantos.quintanilha@gmail.com>
2022-11-09 02:15:42 -0300

    Create scripts.js

---
+++ fundamentos/secao-04-introducao-javascript/variaveis/scripts.js
@@ -0,0 +1,11 @@
+const myName = "Carolina";
+const birthCity = "Araruama";
+let birthYear = 1995;
+birthYear = 2030;
+birthCity = "Rio de Janeiro";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+//item 6 - Variável do tipo const, não atribiu valor, somente variável do tipo let atribui valor!!//
